545e84ec2cf9f3af18f614bf1c6013fbcc5a5a61
BahramRousta <rousta.bahram@gmail.com>
2022-09-22 19:31:49 +0330

    add scan network devices function

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, change_ip_address
+from .views import index, change_ip_address, scan_network
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('change_ip_address/', change_ip_address, name='change_ip_address'),
+    path('scan_network/', scan_network, name='scan_network'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+import time
+
+import yaml
 from django.shortcuts import render, HttpResponse
 from .utils import SSHConnect
 
@@ -5,6 +8,39 @@ from .utils import SSHConnect
 def index(request):
     hostname = "192.168.1.85"
     return render(request, 'core/index.html', {'hostname': hostname})
+
+
+def scan_network(request):
+    device = SSHConnect(username='root',
+                        password='bahram1371',
+                        hostname='192.168.1.31')
+    remote = device.open_session()
+    cmd = remote.invoke_shell()
+    cmd.send(f'nmap -sn 192.168.1.1/24\n')
+    time.sleep(5)
+    output = cmd.recv(50000).decode()
+    print(output)
+    device.close_session()
+
+    machine = []
+    virtual_machine = []
+    for line in output.split('\r'):
+        if "Nmap scan report for 192.168.1." in line:
+            machine.append(line)
+        elif "virtual-machine (192.168.1." in line:
+            virtual_machine.append(line)
+
+    virtual_machine_ip_address = []
+    for index in virtual_machine:
+        a = index.replace('\nNmap scan report for bahram-virtual-machine (', '')
+        b = a.replace(')', '')
+        virtual_machine_ip_address.append(b)
+
+    machine_ip_address = []
+    for index in machine:
+        a = index.replace('\nNmap scan report for ', '')
+        machine_ip_address.append(a)
+    return render(request, 'core/scan_network.html', {'machine': machine_ip_address})
 
 
 def change_ip_address(request):
