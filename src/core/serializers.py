f266eeec2c9a453f23898eef5979d56a66092034
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-11-01 20:59:16 +0330

    add ChangeIPSerializer serializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -18,12 +18,17 @@ class DeviceNetworkSerializer(HostSerializer):
     dns = serializers.ListField(max_length=25)
 
 
+class ChangeIPSerializer(serializers.Serializer):
+    current_ip = serializers.IPAddressField()
+    new_ip = serializers.IPAddressField()
+
 class DNSSerializer(HostSerializer):
     dns = serializers.ListField(max_length=25)
 
 
 class PortSerializer(serializers.Serializer):
     port = serializers.IntegerField()
+
 
 class SSHKeySerializer(serializers.Serializer):
     host = serializers.IPAddressField(required=True)
