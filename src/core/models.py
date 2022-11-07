4836d314439b08057d5abab960f6bbda093aa135
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2022-11-07 21:13:20 +0330

    add device network name to host model

--- src/core/models.py
+++ src/core/models.py
@@ -10,6 +10,7 @@ class Host(models.Model):
     os = models.CharField(max_length=100,
                           null=True,
                           blank=True)
+    network_card_name = models.CharField(max_length=25, null=True, blank=True)
 
     def __str__(self):
         return f"{self.username} - {self.ip_address}"
